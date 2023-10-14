744abce02e3f4e8db8a102fcb18d6290f8fb5fe6
EduardoUresti <eurestiintro@gmail.com>
2023-10-14 12:58:54 -0600

    Practica 8: Action Sheet y Sliding Option

--- src/app/lista-alumnos/lista-alumnos.component.ts
+++ src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
-  styleUrls: ['./lista-alumnos.component.scss'],
+  styleUrls: ['./lista-alumnos.component.scss']
 })
-export class ListaAlumnosComponent  implements OnInit {
+export class ListaAlumnosComponent {
+  alumnos: string[] = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 
-  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
-  constructor() { }
+  constructor(public actionSheetController: ActionSheetController) {}
 
-  ngOnInit() {}
+  addToFavorites(alumno: string) {
+    // Lógica para agregar a favoritos aquí
+    console.log(`Agregado a favoritos: ${alumno}`);
+  }
 
+  async showActionSheet(alumno: string) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Eliminar',
+          role: 'destructive',
+          handler: () => {
+            this.removeAlumno(alumno);
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelado');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
+
+  removeAlumno(alumno: string) {
+    // Lógica para eliminar al alumno aquí
+    const index = this.alumnos.indexOf(alumno);
+    if (index > -1) {
+      this.alumnos.splice(index, 1);
+    }
+  }
 }
