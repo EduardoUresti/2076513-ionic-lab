7680399597d7986f70ffea64cb8569707454844a
EduardoUresti <eurestiintro@gmail.com>
2023-10-28 13:17:38 -0600

    Practica 11: Formularios v2

--- src/app/lista-alumnos/lista-alumnos.component.ts
+++ src/app/lista-alumnos/lista-alumnos.component.ts
@@ -8,46 +8,14 @@ import { Alumno } from '../alumno.model';
   styleUrls: ['./lista-alumnos.component.scss']
 })
 export class ListaAlumnosComponent {
-  alumnos: string[] = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 
   nuevoAlumno: Alumno = new Alumno('', false);
-
-  constructor(public actionSheetController: ActionSheetController) {}
-
-  addToFavorites(alumno: string) {
-    // Lógica para agregar a favoritos aquí
-    console.log(`Agregado a favoritos: ${alumno}`);
-  }
-
-  async showActionSheet(alumno: string) {
-    const actionSheet = await this.actionSheetController.create({
-      header: 'Opciones',
-      buttons: [
-        {
-          text: 'Eliminar',
-          role: 'destructive',
-          handler: () => {
-            this.removeAlumno(alumno);
-          }
-        },
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancelado');
-          }
-        }
-      ]
-    });
-
-    await actionSheet.present();
-  }
-
-  removeAlumno(alumno: string) {
-    // Lógica para eliminar al alumno aquí
-    const index = this.alumnos.indexOf(alumno);
-    if (index > -1) {
-      this.alumnos.splice(index, 1);
+  alumnos: Alumno[] = [];
+  
+  agregarAlumno() {
+    if (this.nuevoAlumno.nombre) {
+      this.alumnos.push(this.nuevoAlumno);
+      this.nuevoAlumno = new Alumno('', false);
     }
   }
 }
