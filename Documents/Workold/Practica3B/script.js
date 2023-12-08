c534dbd089a835e37a9b3d472a69a8d5115df605
EduardoUresti <eurestiintro@gmail.com>
2023-12-07 21:34:49 -0600

    Practica 4

--- Documents/Workold/Practica3B/script.js
+++ Documents/Workold/Practica3B/script.js
@@ -1,31 +1,24 @@
-const users = [];
+let registeredUser = "";
+const validUsername = "usuario";
+const validPassword = "contraseña";
 
-function registro() {
-  const userinput = document.getElementById("userinput").value;
-  const password = document.getElementById("password").value;
+function verifyLogin() {
+  const usernameInput = document.getElementById("username").value;
+  const passwordInput = document.getElementById("password").value;
 
-  if (userinput && password) {
-    const existingUser = users.find(user => user.userinput === userinput);
-    if (existingUser) {
-      document.getElementById("message").textContent = `El usuario ${userinput} ya existe`;
-    } else {
-      users.push({ userinput, password });
-      document.getElementById("message").textContent = `Registro completo, ${userinput}!`;
-    }
+  if (usernameInput === validUsername && passwordInput === validPassword) {
+    login();
   } else {
-    document.getElementById("message").textContent = "Llenar campos";
+    document.getElementById("message").textContent = "Login incorrecto";
   }
 }
 
 function login() {
-  const userinput = document.getElementById("userinput").value;
-  const password = document.getElementById("password").value;
+  document.getElementById("message").textContent = "Login correcto";
+}
 
-  const foundUser = users.find(user => user.userinput === userinput && user.password === password);
-
-  if (foundUser) {
-    document.getElementById("message").textContent = `Login exitoso ${userinput}`;
-  } else {
-    document.getElementById("message").textContent = "Usuario o password equivocado";
-  }
+function registro() {
+  const username = document.getElementById("username").value;
+  registeredUser = username;
+  document.getElementById("message").textContent = "Registro correcto. Usuario: " + username;
 }
